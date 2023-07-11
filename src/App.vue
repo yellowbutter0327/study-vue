9f904e6a7a041f0aec8faafb3a74db9b6335b3ad
joeunhye <slrkwhdmsp@naver.com>
2023-07-11 16:25:21 +0900

    코딩애플1 / 실제 데이터로 상품 목록을 만들자(import, export)

--- src/App.vue
+++ src/App.vue
@@ -13,40 +13,69 @@
     </div>
 
     <div>
-      <img src="./assets/room0.jpg" class="room-img" />
+      <img :src="원룸들[0].image" class="room-img" />
       <h4 class="red" :style="style1" @click="isOpen = true">
-        {{ products[0] }}
+        {{ 원룸들[0].title }}
       </h4>
-      <p>50만원</p>
+      <p>{{ 원룸들[0].price }}원</p>
       <button @click="reportNumber[0] += 1">허위매물신고</button>
       <span>신고수 : {{ reportNumber[0] }}</span>
     </div>
+
     <div>
-      <img src="./assets/room1.jpg" class="room-img" />
+      <img :src="원룸들[1].image" class="room-img" />
       <h4 class="red" :style="style1" @click="isOpen = true">
-        {{ products[1] }}
+        {{ 원룸들[1].title }}
       </h4>
-      <p>60만원</p>
+      <p>{{ 원룸들[1].price }}원</p>
       <button @click="reportNumber[1] += 1">허위매물신고</button>
       <span>신고수 : {{ reportNumber[1] }}</span>
     </div>
+
     <div>
-      <img src="./assets/room2.jpg" class="room-img" />
+      <img :src="원룸들[2].image" class="room-img" />
       <h4 class="red" :style="style1" @click="isOpen = true">
-        {{ products[2] }}
+        {{ 원룸들[2].title }}
       </h4>
-      <p>70만원</p>
+      <p>{{ 원룸들[2].price }}원</p>
       <button @click="reportNumber[2] += 1">허위매물신고</button>
       <span>신고수 : {{ reportNumber[2] }}</span>
+    </div>
+
+    <div>
+      <img :src="원룸들[3].image" class="room-img" />
+      <h4 class="red" :style="style1" @click="isOpen = true">
+        {{ 원룸들[3].title }}
+      </h4>
+      <p>{{ 원룸들[3].price }}원</p>
+    </div>
+
+    <div>
+      <img :src="원룸들[4].image" class="room-img" />
+      <h4 class="red" :style="style1" @click="isOpen = true">
+        {{ 원룸들[4].title }}
+      </h4>
+      <p>{{ 원룸들[4].price }}원</p>
+    </div>
+
+    <div>
+      <img :src="원룸들[5].image" class="room-img" />
+      <h4 class="red" :style="style1" @click="isOpen = true">
+        {{ 원룸들[5].title }}
+      </h4>
+      <p>{{ 원룸들[5].price }}원</p>
     </div>
   </div>
 </template>
 
 <script>
+import data from "./assets/oneroom.js";
+
 export default {
   name: "App",
   data() {
     return {
+      원룸들: data,
       isOpen: true,
       reportNumber: [0, 0, 0],
       price1: [60, 50, 70],
