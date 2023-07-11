e4f7c996850056ced3e6984065f16a8606be77a9
joeunhye <slrkwhdmsp@naver.com>
2023-07-11 16:01:58 +0900

    코딩애플 part 1 / v-if와 모달창 만들기 (Vue에서 동적인 UI 만드는 법)

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,43 @@
 <template>
   <div>
+    <div class="black-bg" v-if="isOpen == true">
+      <div class="white-bg">
+        <h4>상세페이지임</h4>
+        <p>상세페이지 내용임</p>
+        <button @click="isOpen = false">닫기</button>
+      </div>
+    </div>
+
     <div class="menu">
       <a v-for="(menuName, i) in menuList" :key="i">{{ menuName }}</a>
     </div>
-    <div v-for="(roomName, i) in roomList" :key="i">
-      <h4 class="red" :style="style1">{{ roomName }}</h4>
-      <p>{{ price1[i] }}만원</p>
-      <button v-on:click="increase">허위매물신고</button>
-      <span>신고수 : {{ 신고수[i] }}</span>
+
+    <div>
+      <img src="./assets/room0.jpg" class="room-img" />
+      <h4 class="red" :style="style1" @click="isOpen = true">
+        {{ products[0] }}
+      </h4>
+      <p>50만원</p>
+      <button @click="reportNumber[0] += 1">허위매물신고</button>
+      <span>신고수 : {{ reportNumber[0] }}</span>
+    </div>
+    <div>
+      <img src="./assets/room1.jpg" class="room-img" />
+      <h4 class="red" :style="style1" @click="isOpen = true">
+        {{ products[1] }}
+      </h4>
+      <p>60만원</p>
+      <button @click="reportNumber[1] += 1">허위매물신고</button>
+      <span>신고수 : {{ reportNumber[1] }}</span>
+    </div>
+    <div>
+      <img src="./assets/room2.jpg" class="room-img" />
+      <h4 class="red" :style="style1" @click="isOpen = true">
+        {{ products[2] }}
+      </h4>
+      <p>70만원</p>
+      <button @click="reportNumber[2] += 1">허위매물신고</button>
+      <span>신고수 : {{ reportNumber[2] }}</span>
     </div>
   </div>
 </template>
@@ -17,18 +47,15 @@ export default {
   name: "App",
   data() {
     return {
-      신고수: [0, 0, 0],
+      isOpen: true,
+      reportNumber: [0, 0, 0],
       price1: [60, 50, 70],
       style1: "color:red",
-      roomList: ["역삼동원룸", "천호동원룸", "마포구원룸"],
+      products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
       menuList: ["Home", "Shop", "About"],
     };
   },
-  methods: {
-    increase() {
-      this.신고수 += 1;
-    },
-  },
+  methods: {},
   components: {},
 };
 </script>
@@ -42,6 +69,26 @@ export default {
   color: #2c3e50;
 }
 
+body {
+  margin: 0;
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+}
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+
 .menu {
   background: darkslateblue;
   padding: 15px;
@@ -51,5 +98,10 @@ export default {
 .menu a {
   color: white;
   padding: 10px;
+}
+
+.room-img {
+  width: 100%;
+  margin-top: 40px;
 }
 </style>
