53bae9871fc50c1d302c8c9b889825c5d243e5e3
joeunhye <slrkwhdmsp@naver.com>
2023-07-11 11:08:45 +0900

    코딩애플 part 1/vue 반복문 v-for

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <template>
   <div>
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <div>
-      <h4 class="red" :style="style1">xx원룸</h4>
-      <p>{{ price1 }}만원</p>
+    <div class="menu">
+      <a v-for="(menuName, i) in menuList" :key="i">{{ menuName }}</a>
     </div>
-    <div>
-      <h4>xx원룸</h4>
-      <p>xx만원</p>
+    <div v-for="(roomName, i) in roomList" :key="i">
+      <h4 class="red" :style="style1">{{ roomName }}</h4>
+      <p>{{ price1[i] }}만원</p>
     </div>
   </div>
 </template>
@@ -17,8 +15,10 @@ export default {
   name: "App",
   data() {
     return {
-      price1: 60,
+      price1: [60, 50, 70],
       style1: "color:red",
+      roomList: ["역삼동원룸", "천호동원룸", "마포구원룸"],
+      menuList: ["Home", "Shop", "About"],
     };
   },
 };
@@ -31,6 +31,16 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color: white;
+  padding: 10px;
 }
 </style>
